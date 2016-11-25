e481e14ded03f739073a22025ab6000fb4731246
Monika Yadav <monika.yadav@delhivery.com>
2016-11-25 18:06:19 +0530

    new files added

---
+++ oddeven.py
@@ -0,0 +1,6 @@
+my_list = [2,7,8,4,11]
+for i in my_list:
+	if i % 2 == 0:
+		print "%s is even %s" % (i, i+1)
+	else:
+		print "%s is odd" % i
---
+++ table.py
@@ -0,0 +1,17 @@
+x = int(raw_input("Enter a number b/w 2 to 20    "))
+for i in range(1, 11):
+	c = x * i
+	print x , "*",  i , "=" , c
+	
+
+
+
+	# y = i[0] + i[1]
+	
+# a , b = 1, 1
+# for i in range(9):
+# 	a,b = b, a+b
+# 	print a
+
+# x = [1,2,3,4]
+# print sum(x)
